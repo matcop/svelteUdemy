d60974690f3806104c29f3f1d61654b6b78dee3e
matcop <matcop@gmail.com>
2020-07-27 23:27:57 -0400

    1-18 Diving Deeper into bindings

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,61 @@
 <script>
-	export let name;
+import Contactcard from './Contactcard.svelte';
+  export let name;
+  export let age;
+console.log(age);
+$:uppercaseName=name.toUpperCase();
+$:console.log(name);
+$:if(name=='Maximo Decimo'){
+	age=100;
+}
+
+	function incrementAge() {
+    age += 1;
+	};
+	function changeName(){
+		name="Maximo Decimo"
+	};
+	function nameInput(event){
+		const enterValue=event.target.value;
+		name=enterValue;
+	}
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+<main>
+  <h3>Hello {uppercaseName}, and my age is {age}</h3>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <button on:click={incrementAge}>increment</button>
+	<!-- <button on:click={changeName}>Change Name</button> -->
+	<br>
+	<!-- <input type="text" value={name} on:input={nameInput}> -->
+	<input type="text" bind:value={name}/>
+
+
+<Contactcard userName={name}/>
+
+
+</main>
